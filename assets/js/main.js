252e6a347cd92c44b50e6b745fd90722d40e80ed
alifaizan786-op <faizan.ali@irunadigital.com>
2021-09-18 20:33:15 -0400

    added questions and ade questions display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,11 @@
 var timer = document.querySelector (".timer")
 var start = document.querySelector (".start")
-var question = document.querySelector (".question")
+var questionEl = document.querySelector (".question")
 var choice = document.querySelector (".choice")
 var highscore = document.querySelector (".highscore")
+var questionIndex
 
-start.addEventListener("click", function (event) {
-    startGame();
-    countdown();
-
-});
-
-// function endGame() {
-//     $("highscore").trigger('click');
-// }
-
-
-function startGame() {
-    question.classList.remove('hide')
-    choice.classList.remove('hide')
-    start.classList.add('hide')
-
-}
-
-
-function countdown() {
-    var timeLeft = 10;
-    var timeInterval = setInterval(function () {
-        timeLeft--;
-        timer.textContent = timeLeft;
-
-        if (timeLeft === 0) {
-            clearInterval(timeInterval);
-            timer.textContent = "";
-            // endGame();
-        }
-    }, 1000);
-}
+console.log(questionEl);
 
 const questions = [
     {
@@ -48,6 +18,13 @@ const questions = [
       ]
     },
     {
+        question: 'Is "64" & 64 the same in javascript ?',
+        answers: [
+          { text: 'Yes', correct: false },
+          { text: 'No', correct: true }
+        ]
+    },
+    {
       question: 'What is the correct way to stop event bubbling?',
       answers: [
         { text: 'event.stopPropogation()', correct: true },
@@ -56,4 +33,79 @@ const questions = [
         { text: 'stop.Bubbling', correct: false }
       ]
     },
-  ]
+    {
+        question: 'What is the correct Symbol for "OR" comparision ?',
+        answers: [
+          { text: '&&', correct: false },
+          { text: '=>', correct: false },
+          { text: '-', correct: false },
+          { text: '||', correct: true }
+        ]
+    },
+    {
+        question: 'Is Javascript & Java the same ?',
+        answers: [
+          { text: 'true', correct: false },
+          { text: 'false', correct: true }
+        ]
+    },
+    {
+        question: 'What is the correct method to add items to a array ?',
+        answers: [
+          { text: '.slice', correct: false },
+          { text: '.splice', correct: false },
+          { text: '.push', correct: true },
+          { text: '.pull', correct: false }
+        ]
+    },
+]
+
+start.addEventListener("click", function (event) {
+    startQuiz();
+    countdown();
+
+});
+
+function startQuiz() {
+    questionEl.classList.remove('hide')
+    choice.classList.remove('hide')
+    start.classList.add('hide')
+    randQuestion()
+    showQuestion()
+}
+
+
+function randQuestion() {
+    return questions[Math.floor(Math.random() * questions.length)]
+}
+
+
+
+function showQuestion() {
+    questionEl.innerText = randQuestion().question
+    const button = document.createElement ('button')
+    for (var i = 0; i < randQuestion().answers.length; i++){
+        button.dataset.correct = randQuestion().answers.correct
+        button.addEventListener('click', selectAnswer)
+        choice.appendChild(button)
+    }
+}
+
+function selectAnswer(){
+    
+}
+
+function countdown() {
+    var timeLeft = 10;
+    var timeInterval = setInterval(function () {
+        timeLeft--;
+        timer.textContent = timeLeft;
+
+        if (timeLeft === 0) {
+            clearInterval(timeInterval);
+            timer.textContent = "";
+            //other functions
+        }
+    }, 1000);
+}
+
