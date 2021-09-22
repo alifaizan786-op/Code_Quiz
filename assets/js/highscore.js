9ec52d817158d4c386a8a9a55258ac4c57935eae
alifaizan786-op <faizan.ali@irunadigital.com>
2021-09-22 17:50:14 -0400

    final code pushed

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,4 +1,4 @@
-var highScoreList = JSON.parse(localStorage.getItem(".scoresList"))
+var highScoreList = JSON.parse(localStorage.getItem("scoresList"))
 var clearscore = document.querySelector(".clearscore")
 console.log(highScoreList)
 var listEl = document.querySelector(".listEl")
@@ -7,6 +7,8 @@ clearscore.addEventListener("click", function(event){
     localStorage.clear();
 })
 
-for (var i; i < highScoreList.length; i++){
-    listEl.innerHTML += '<tr><td>' + highScoreList[i].playerName + '</td><td>' + highScoreList[i].timeTake + '</td><td>' + highScoreList.score + '</td></tr>'
+console.log(highScoreList);
+
+for (var i = 0 ; i < highScoreList.length; i++){
+    listEl.innerHTML += '<tr><td>' + highScoreList[i].playerName + '</td><td>' + highScoreList[i].timeTake + '</td><td>' + highScoreList[i].score + '</td></tr>'
 }
